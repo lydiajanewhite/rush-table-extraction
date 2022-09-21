62727f705b8e6f7af565e1803d071416754b99d6
Lydia Jane White <lydiaw140@gmail.com>
2022-09-21 14:32:44 +0300

    extracted another study

--- analysis/importing and tidying.R
+++ analysis/importing and tidying.R
@@ -96,4 +96,39 @@ wider <- data %>%
   filter(year == "2005") %>% 
   pivot_wider(names_from = taxa, values_from = cover)
 
+# 162
+data <- read_csv(here::here("data","452_White2018_invertalgae_abundance_td.csv"))
+head(data)
+unique(data$Predators)
+## change treatment names, and make single treatment by joining stressor variables 
+## temperature:  ambient, heated -> T0 and T1
+## nutrients: ambient, enriched -> T0 and T1 N0 and N1 
+## predators: remove spaces and capitals none, crab, whelk, crab_whelk
 
+data<-data%>%
+  mutate(Temperature=recode(Temperature, Ambient ="T0", Heated ="T1"))%>%
+  mutate(Nutrient=recode(Nutrient, Ambient ="N0", Enriched="N1")) %>% 
+  mutate(Predators =recode(Predators, None = "none", Crab ="crab", Whelk ="whelk", 'Crab & Whelk' ="crabwhelk")) %>% 
+  unite("group_id", Temperature, Nutrient, Predators)
+
+#  then summarise mean sp abundance across treatments and pivot
+
+mean <-data %>% 
+  group_by (group_id) %>% 
+  summarise_at(vars('serratus':'caprella sp'), mean) %>% 
+  pivot_longer(!group_id, names_to = "variable", values_to = "mean")
+
+sd<-data %>% 
+  group_by (group_id) %>% 
+  summarise_at(vars('serratus':'caprella sp'),sd) %>% 
+  pivot_longer(!group_id, names_to = "variable", values_to = "sd")
+
+n<-data %>% 
+  group_by (group_id) %>% 
+  summarise_at(vars('serratus':'caprella sp'), length) %>% 
+  pivot_longer(!group_id, names_to = "variable", values_to = "n")
+
+summary_452<- inner_join(mean,sd) %>% 
+  inner_join(., n)
+
+summary_452$filename <- '452_White2018_invertalgae_abundance'
