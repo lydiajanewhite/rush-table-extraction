abf56776e91d435459d1fbf9b4bff24efa96a65d
Lydia Jane White <lydiaw140@gmail.com>
2022-09-26 10:10:48 +0300

    tidy script

--- analysis/importing /import_179.R
+++ analysis/importing /import_179.R
@@ -4,7 +4,6 @@ library(tidyverse)
 # 179
 
 data <- read_csv(here::here("data","179_Brandt2019_plant_cover_data.csv"))
-biomass_data <- read_csv(here::here("data","179_Brandt2019_plant_biomass_data.csv"))
 
 head(data)
 unique(data$unit)
@@ -14,41 +13,21 @@ unique(data$year)
 ## change treatment names, and make single treatment by joining stressor variables 
 ## ntrt: CONTROL = ambient, nutrient = enriched, organic_matter = organic -> N0_O0 and N1_O0 and N0_O1
 ## disturbance: 0 = ambient, 1= physical disturbance,  -> PD0 and PD1
-## 
-a <- biomass_data %>%  filter (unit == "2" & year == "2006") %>% 
-  group_by(taxa) %>% 
-  summarise (mean = mean(mass))
-
-
-b <- data %>%  filter (unit == "2" & year == "2006")%>% 
-  group_by(taxa) %>% 
-  summarise (mean = mean(cover))
 
 data <- data %>% 
   mutate(nutrient_OM =recode(ntrt, 'CONTROL' ="N0_O0", "#NAME?" ="N1_O0","+C"="N0_O1")) %>% 
   separate(nutrient_OM, c("nutrient", "organic_matter")) %>%
   mutate(disturbance=recode(dist, "0" ="PD0", "1"="PD1")) %>% 
   unite("group_id", nutrient, organic_matter, disturbance) %>% 
-  unite("location_id", site, block, plot)
-
-#  then summarise mean sp abundance across treatments and pivot
-
-head(data)
-unique(data$group_id)
-unique(data$unit)
-unique(data$location_id)
-
-data1 <- data %>% 
-  select(group_id, unit, year, taxa, cover) %>% 
-  group_by(group_id, unit, year, taxa) %>% 
-  summarise(cover= mean(cover))
-
-wide <- pivot_wider(data1, names_from = taxa, values_from = cover) 
-
+  unite("location_id", site, block, plot, remove = F)
 
 data <- data %>% 
-  select(group_id, unit, year, taxa, cover) 
+  select( block, group_id, unit, year, taxa, cover) 
 
-wider <- data %>% 
+wide<- data %>% 
   pivot_wider(names_from = taxa, values_from = cover)
 
+# how many cases of n > 1? 
+test <-data %>%  dplyr::group_by(group_id, block, unit, year, taxa) %>%
+  dplyr::summarise(n = dplyr::n(), .groups = "drop") %>%
+  dplyr::filter(n > 1L) 
